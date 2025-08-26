90497cf1eb07ca02bfac261468ba9adde049e53f
AndreasDevanPanduNarasamdya <andreasdevan123@gmail.com>
2025-08-26 21:28:38 +0700

    yeah

--- Assignment3/FloatingPoint/src/FloatingPoint.java
+++ Assignment3/FloatingPoint/src/FloatingPoint.java
@@ -1,6 +1,7 @@
-Class FloatingPoint{
+public class FloatingPoint{
     public static void main(String[] args) {
     double x = 92.98;
     int nx = (int) Math.round(x);
+    System.out.println(nx);
     }
 }
